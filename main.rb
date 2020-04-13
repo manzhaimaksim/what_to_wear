cd33a98495bd53bbff07a6dd4effa37b06467dac
manzhaimaksim <maxbbb88@gmail.com>
2020-04-13 23:57:58 +0300

    Adding program execution to the main file

--- main.rb
+++ main.rb
@@ -1,10 +1,16 @@
 require_relative 'lib/thing'
+require_relative 'lib/collection_of_things'
 
-things_path = __dir__ + '/data'
-things = Thing.things_from_folder(things_path)
+things_paths = Dir[__dir__ + '/data/*.txt']
+things = Thing.things_from_folder(things_paths)
+
+collection = CollectionOfThings.new(things)
 
 puts 'Сколько градусов сегодня за окном? (можно с минусом)'
 user_input = STDIN.gets.to_i
 
+collection.what_to_wear(user_input)
 puts 'Предлагаем сегодня надеть:'
-puts "#{user_input}"
+puts
+
+collection.wear_for_user.each { |thing| puts thing }
