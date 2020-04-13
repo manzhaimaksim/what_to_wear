7a6361592879201f5b060035b5e1502cc48914ad
manzhaimaksim <maxbbb88@gmail.com>
2020-04-13 11:20:21 +0300

    add class thing

---
+++ lib/thing.rb
@@ -0,0 +1,10 @@
+class Thing
+  def self.things_from_folder(folder_path)
+  end
+
+  def initialize(params)
+    @title = params[:title]
+    @category = params[:category]
+    @temp_range = params[:temp_range]
+  end
+end
