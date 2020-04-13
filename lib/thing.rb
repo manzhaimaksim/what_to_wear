040d3c427d4a2728fc9bfbf34f533f15311e6891
manzhaimaksim <maxbbb88@gmail.com>
2020-04-13 15:16:50 +0300

    Adding self method for creating Thing

--- lib/thing.rb
+++ lib/thing.rb
@@ -1,5 +1,11 @@
 class Thing
-  def self.things_from_folder(folder_path)
+  def self.things_from_folder(paths)
+    paths.each do |path|
+      lines = File.readlines(path, chomp: true, encoding: 'UTF-8')
+      params = {title: lines[0], category: lines[1], temp_range: lines[2]}
+      self.new(params)
+    end
+
   end
 
   def initialize(params)
