5858038d45aa07858a68f75f9926c90bd3d159a2
manzhaimaksim <maxbbb88@gmail.com>
2020-04-13 17:03:42 +0300

    Converting a temperature range from a string to a range

--- lib/thing.rb
+++ lib/thing.rb
@@ -1,11 +1,15 @@
 class Thing
   def self.things_from_folder(paths)
+    things = []
     paths.each do |path|
       lines = File.readlines(path, chomp: true, encoding: 'UTF-8')
-      params = {title: lines[0], category: lines[1], temp_range: lines[2]}
-      self.new(params)
-    end
 
+      lines[2] = lines[2].delete('()').split(', ').map(&:to_i)
+
+      params = { title: lines[0], category: lines[1], temp_range: lines[2][0]..lines[2][1] }
+      things << self.new(params)
+    end
+    things
   end
 
   def initialize(params)
