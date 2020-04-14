ab2caeac5f9e2cc91f09987137982086c65b7ede
manzhaimaksim <maxbbb88@gmail.com>
2020-04-14 20:28:39 +0300

    Replacing method things_from_folder from Thing to ColectionOfThing

--- lib/collection_of_things.rb
+++ lib/collection_of_things.rb
@@ -1,14 +1,25 @@
 class CollectionOfThings
   attr_reader :wear_for_user
 
-  def initialize(collection)
-    @collection = collection
-    @categories = @collection.map(&:category).uniq # хранит массив с категориями одежды
+  def initialize
+    @collection = []
+    @categories = []
     @wear_for_user = []
   end
 
+  def things_from_folder(paths)
+    paths.each do |path|
+      lines = File.readlines(path, chomp: true, encoding: 'UTF-8')
+
+      lines[2] = lines[2].delete('()').split(', ').map(&:to_i)
+
+      params = { title: lines[0], category: lines[1], temp_range: lines[2][0]..lines[2][1] }
+      @collection << Thing.new(params)
+    end
+  end
+
   def what_to_wear(temperature)
-    @categories.each do |category|
+    categories_of_things.each do |category|
       @wear_for_user << things_of_same_type(category).select do |thing|
         thing.suitable_for_weather?(temperature)
       end
@@ -17,6 +28,11 @@ class CollectionOfThings
   end
 
   private
+
+  # возвращает массив с категориями одежды
+  def categories_of_things
+    @categories = @collection.map(&:category).uniq
+  end
 
   # возвращает массив с объектами одной категории
   def things_of_same_type(type)
