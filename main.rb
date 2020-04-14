64150ae76f946fe5e42cb99053aad02f414a51d8
manzhaimaksim <maxbbb88@gmail.com>
2020-04-14 20:41:03 +0300

    Edit main program

--- main.rb
+++ main.rb
@@ -2,15 +2,15 @@ require_relative 'lib/thing'
 require_relative 'lib/collection_of_things'
 
 things_paths = Dir[__dir__ + '/data/*.txt']
-things = Thing.things_from_folder(things_paths)
 
-collection = CollectionOfThings.new(things)
+collection = CollectionOfThings.new
+collection.things_from_folder(things_paths)
 
 puts 'Сколько градусов сегодня за окном? (можно с минусом)'
-user_input = STDIN.gets.to_i
-
+# user_input = STDIN.gets.to_i
+user_input = 10
 collection.what_to_wear(user_input)
 puts 'Предлагаем сегодня надеть:'
 puts
 
-collection.wear_for_user.each { |thing| puts thing }
+collection.wear_for_user.each { |thing| puts thing if !thing.nil? }
