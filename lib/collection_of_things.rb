0a131c3d415ab911fd80ce94f3e4204e4ea1b6cd
manzhaimaksim <maxbbb88@gmail.com>
2020-04-16 23:14:43 +0300

    Replace things_from_folder from instance methods to methods of class

--- lib/collection_of_things.rb
+++ lib/collection_of_things.rb
@@ -1,6 +1,14 @@
 class CollectionOfThings
-  def initialize(paths)
-    @collection = things_from_folder(paths)
+  def initialize(things)
+    @collection = things
+  end
+
+  def self.things_from_folder(paths)
+    paths.map do |path|
+      lines = File.readlines(path, chomp: true, encoding: 'UTF-8')
+      params = { title: lines[0], category: lines[1], temp_range: lines[2] }
+      Thing.new(params)
+    end
   end
 
   def what_to_wear(temperature)
@@ -22,13 +30,5 @@ class CollectionOfThings
   # возвращает массив с объектами одной категории
   def things_of_same_type(category)
     @collection.select { |thing| thing.category == category }
-  end
-
-  def things_from_folder(paths)
-    paths.map do |path|
-      lines = File.readlines(path, chomp: true, encoding: 'UTF-8')
-      params = { title: lines[0], category: lines[1], temp_range: lines[2] }
-      Thing.new(params)
-    end
   end
 end
--- main.rb
+++ main.rb
@@ -1,7 +1,9 @@
 require_relative 'lib/thing'
 require_relative 'lib/collection_of_things'
 
-collection = CollectionOfThings.new(Dir[__dir__ + '/data/*.txt'])
+things_paths = Dir[__dir__ + '/data/*.txt']
+things = CollectionOfThings.things_from_folder(things_paths)
+collection_of_thing = CollectionOfThings.new(things)
 
 puts 'Сколько градусов сегодня за окном? (можно с минусом)'
 user_input = STDIN.gets.to_i
@@ -9,4 +11,4 @@ user_input = STDIN.gets.to_i
 puts 'Предлагаем сегодня надеть:'
 puts
 
-puts collection.what_to_wear(user_input)
+puts collection_of_thing.what_to_wear(user_input)
