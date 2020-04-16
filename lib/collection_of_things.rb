af31c6ec59d8e9e97def5a27a393ca7d772077b6
manzhaimaksim <maxbbb88@gmail.com>
2020-04-16 17:50:20 +0300

    Edit what_to_wear method and replace method things_from_folder to private scope

--- lib/collection_of_things.rb
+++ lib/collection_of_things.rb
@@ -3,18 +3,10 @@ class CollectionOfThings
     @collection = things_from_folder(paths)
   end
 
-  def things_from_folder(paths)
-    paths.map do |path|
-      lines = File.readlines(path, chomp: true, encoding: 'UTF-8')
-      params = { title: lines[0], category: lines[1], temp_range: lines[2] }
-      Thing.new(params)
-    end
-  end
-
   def what_to_wear(temperature)
-    categories_of_things.map do |category|
+    categories_of_things.filter_map do |category|
       things_of_same_type(category).select do |thing|
-        thing.suitable_for_weather?(temperature)
+        thing.suitable_for_weather?(temperature) unless thing.nil?
       end
       .sample
     end
@@ -30,5 +22,13 @@ class CollectionOfThings
   # возвращает массив с объектами одной категории
   def things_of_same_type(category)
     @collection.select { |thing| thing.category == category }
+  end
+
+  def things_from_folder(paths)
+    paths.map do |path|
+      lines = File.readlines(path, chomp: true, encoding: 'UTF-8')
+      params = { title: lines[0], category: lines[1], temp_range: lines[2] }
+      Thing.new(params)
+    end
   end
 end
